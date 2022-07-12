597cb178dee881f49472fcb30871ae8cb1cb687a
jose <joseluis836@hotmail.com>
2022-07-12 09:11:40 -0500

    Se creo el sistema de rutas para manejar la App. App.jsx cambio de directorio a routes. Se instalo react-routes-dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './routes/App';
 
 
 ReactDOM.createRoot(document.getElementById('app')).render(
